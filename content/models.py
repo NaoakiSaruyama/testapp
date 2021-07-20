91f152b6f212397ed60840763c7c862aa2d2af03
NaoakiSaruyama <saruyanamaoaki3412@gmail.com>
2021-07-20 22:22:21 +0900

    change url

--- content/models.py
+++ content/models.py
@@ -8,6 +8,6 @@ from django.db.models.fields import CharField, DateTimeField
 #ユーザー登録
 class User(models.Model):
   name = models.CharField(verbose_name='ユーザー名',max_length=15)
-  key = models.CharField(verbose_name='ユーザーid',max_length=30)
+  email = models.EmailField(verbose_name='ユーザーid',max_length=30)
   password = models.CharField(verbose_name='パスワード',max_length=15)
   regist_date = models.DateTimeField(default=timezone.now)
